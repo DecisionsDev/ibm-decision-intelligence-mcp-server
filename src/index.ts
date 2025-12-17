1530c2b8fa330fc0f8e388413441d3da344b1217
Benjamin Ratiarisolo <benjamin.ratiarisolo@fr.ibm.com>
2025-12-17 17:15:23 +0100

    Fix error TS2441: Duplicate identifier 'require'. Compiler reserves name 'require' in top level scope of a module.

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,8 @@ import path from 'path';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-const require = createRequire(import.meta.url);
-const packageJson = require(path.join(dirname, '../package.json'));
+const requireModule = createRequire(import.meta.url);
+const packageJson = requireModule(path.join(dirname, '../package.json'));
 
 const configuration = createConfiguration(packageJson.version);
 const serverName = "mcp-server";
